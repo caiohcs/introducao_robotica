b1c641bba051d757466905dfa147b668c1e27ff8
Caio Henrique <caio.lief@gmail.com>
2016-10-20 19:30:54 -0200

    Deixei apenas a declaração local das variáveis gc_preto e gc_branco

--- master/bibliotecas/GC.h
+++ master/bibliotecas/GC.h
@@ -2,11 +2,10 @@
 
 #ifndef GC_H
 #define GC_H
-GC gc_preto, gc_branco;
 
 GC gc_pintar (Display *disp, Window win, unsigned long valuesmask, XGCValues *values)
 {
- gc_preto = XCreateGC(disp, win, valuesmask, values);
+ GC gc_preto = XCreateGC(disp, win, valuesmask, values);
  XSetForeground(disp,gc_preto,XBlackPixel(disp, DefaultScreen(disp)));
  XSetBackground(disp,gc_preto,XBlackPixel(disp, DefaultScreen(disp)));
  return gc_preto;
@@ -14,7 +13,7 @@ GC gc_pintar (Display *disp, Window win, unsigned long valuesmask, XGCValues *va
 
 GC gc_apagar (Display *disp, Window win, unsigned long valuesmask, XGCValues *values)
 {
- gc_branco = XCreateGC(disp, win, valuesmask, values);
+ GC gc_branco = XCreateGC(disp, win, valuesmask, values);
  XSetForeground(disp,gc_branco,XWhitePixel(disp, DefaultScreen(disp)));
  XSetBackground(disp,gc_branco,XWhitePixel(disp, DefaultScreen(disp)));
  return gc_branco;
