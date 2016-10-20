f3bc4b7bf9bece65ce9a09445966963dbdb8b273
Emanoel Victor <victoremanoel23@gmail.com>
2016-10-20 19:18:28 -0300

    Criação de GC.h para configurar os GCs do programa

---
+++ master/bibliotecas/GC.h
@@ -0,0 +1,23 @@
+#include <X11/Xlib.h>
+
+#ifndef GC_H
+#define GC_H
+GC gc_preto, gc_branco;
+
+GC gc_pintar (Display *disp, Window win, unsigned long valuesmask, XGCValues *values)
+{
+ gc_preto = XCreateGC(disp, win, valuesmask, values);
+ XSetForeground(disp,gc_preto,XBlackPixel(disp, DefaultScreen(disp)));
+ XSetBackground(disp,gc_preto,XBlackPixel(disp, DefaultScreen(disp)));
+ return gc_preto;
+}
+
+GC gc_apagar (Display *disp, Window win, unsigned long valuesmask, XGCValues *values)
+{
+ gc_branco = XCreateGC(disp, win, valuesmask, values);
+ XSetForeground(disp,gc_branco,XWhitePixel(disp, DefaultScreen(disp)));
+ XSetBackground(disp,gc_branco,XWhitePixel(disp, DefaultScreen(disp)));
+ return gc_branco;
+}
+
+#endif
--- master/main.c
+++ master/main.c
@@ -9,6 +9,7 @@
 #include "./bibliotecas/calculos_angulos.h"
 #include "./bibliotecas/cinematica.h"
 #include "./bibliotecas/textos.h"
+#include "./bibliotecas/GC.h"
 #include <math.h>
 #include <string.h>
 
@@ -19,9 +20,11 @@
 #define L2 18.7325
 #define L3 10.0
 
-int coord_real_X_to_pixels(float X, unsigned int larg_main_win){return (X + (larg_main_win/2));} //pega X (real) em centimetros e dá em pixels da tela
+/*Converte X e Y, reais (em centímetros), em um número de pixels na tela*/
 
-int coord_real_Y_to_pixels(float Y, unsigned int alt_main_win){return ((alt_main_win/2)-Y);}	//pega Y (real) em centimetros e dá em pixels da tela
+int coord_real_X_to_pixels(float X, unsigned int larg_main_win){return (X + (larg_main_win/2));}
+
+int coord_real_Y_to_pixels(float Y, unsigned int alt_main_win){return ((alt_main_win/2)-Y);}
 
 void desenha_escala(Display *disp, int tela, Window janela, unsigned int altjanela, unsigned int largjanela)
 {
@@ -154,15 +157,10 @@ int main()
 	XGCValues values;
 	unsigned long valuemask = 0;
 
-	GC gc_branco;		// pinta de branco (mesma cor do fundo) pra apagar
-	gc_branco = XCreateGC(display, main_win, valuemask, &values);
-	XSetForeground(display, gc_branco, white);
-	XSetBackground(display, gc_branco, white);
-	
-	GC gc_preto;	// pinta de preto pra desenhar
-	gc_preto = XCreateGC(display, main_win, valuemask, &values);
-	XSetForeground(display, gc_preto, black);
-	XSetBackground(display, gc_preto, white);
+	/*Configuração dos GCs para pintar a apagar na janela do programa */
+	GC gc_branco, gc_preto;
+	gc_preto = gc_pintar(display, main_win, valuemask, &values);
+	gc_branco = gc_apagar(display, main_win, valuemask, &values);
 
 	XDrawArc(display,main_win,gc_preto,coord_real_X_to_pixels(CY*7,larg_main_win)-5,coord_real_Y_to_pixels((-7)*CX,alt_main_win)-5,10,10,0,360*64);
 	desenhar_eixos(display, main_win, larg_main_win, alt_main_win, gc_preto);
