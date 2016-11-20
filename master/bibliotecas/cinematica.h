c0cf510a4a5a9f20f99d728f168468635d1dd535
Caio Henrique <caio.lief@gmail.com>
2016-11-20 20:39:23 -0200

    Anotações sobre cinematica para entender melhor

--- master/bibliotecas/cinematica.h
+++ master/bibliotecas/cinematica.h
@@ -57,7 +57,10 @@ float *ptr_angles(float Xuser,float Yuser,float Zuser,float PHIuser)	// calcula 
 	A = Xuser/cos(P[0]) - L3*cos(PHIuser);
 	B = Zuser - L3*sin(PHIuser) - alt;
 
-	//Em rad, a funcao acos retorna valores entre 0 e pi, mas o angulo do cotovelo é limitado a um intervalo negativo, então multiplica-se acos por -1
+	/* Em rad, a funcao acos retorna valores entre 0 e pi, mas o angulo do cotovelo é limitado a um intervalo negativo, então multiplica-se acos por -1
+	 * Ou seja, não há ambiguidade nos valores do angulo do cotovelo, já que os únicos valores possíveis eram um no intervalo (0, pi) e outro que
+	 * é o negativo desse, ou seja, no intervalo (0, -pi).
+	 */
 	P[2] = (-1)*acos( (pow(A,2) + pow(B,2) - pow(L1,2) - pow(L2,2))/(2*L1*L2) ); 	
 	if (P[2] < cotovelo.angmin || P[2] > cotovelo.angmax || isnan(P[2])){
 		printf("Ang cot invalido: %f\n", P[2]);
@@ -66,9 +69,21 @@ float *ptr_angles(float Xuser,float Yuser,float Zuser,float PHIuser)	// calcula 
 	}
 	C = ( (pow(A,2) + pow(B,2) + pow(L1,2) - pow(L2,2))/(2*L1) );
 	
+	/* 
+	 * Essa é uma resposta, que não sei se é possível, para o angulo do ombro. A função asin retorna valores entre (-pi/2,pi/2), enquanto que a atan2 retorna entre
+	 * (-pi,pi). Para todas as posições que testei, essa resposta não funcionou (repare na mensagem ang omb invalido ao testar uma posicao), deu angulo inválido.
+	 * O intervalo total então é (-3pi/2,3pi/2).
+	 */
 	P[1] = asin(C/sqrt(pow(A,2)+pow(B,2))) - atan2(A,B);
 	if (P[1] < ombro.angmin || P[1] > ombro.angmax || isnan(P[1])){
 		printf("Ang omb invalido: %f\n", P[1]);
+		/*
+		 * Já essa outra resposta pro ombro, pegando o angulo secundário do sin (fazendo pi - asin), funcionou para algumas posições.
+		 * Nesse caso, o intervalo de pi - asin é (pi/2,3pi/2) e o do atan2 continua sendo (-pi,pi).
+		 * Assim, o intervalo total é (-pi/2,5pi/2)
+		 * A limitação física do ombro é (ombro.angmin e ombro.angmax) que dá aproximadamente (1,09;2,66) em radianos.
+		 * Creio que analisando o intervalo total das duas respostas e a da limitação física do ombro seja possível provar que essa é a única possível
+		 */
 		P[1] = M_PI - asin(C/sqrt(pow(A,2)+pow(B,2))) - atan2(A,B);
 		if (P[1] < ombro.angmin || P[1] > ombro.angmax || isnan(P[1])){
 			printf("Ang omb invalido: %f\n", P[1]);
